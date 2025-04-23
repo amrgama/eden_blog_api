238a65f4da4a44e14e0cb5a80ccb828915979599
mohamedfathycreation <m.fathy@creation.sa>
2025-04-23 23:08:30 +0200

    update allow origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = ["http://localhost:5173", "http://localhost:5174", "https://edenblogapi-production.up.railway.app"];
 
 module.exports = allowedOrigins;
