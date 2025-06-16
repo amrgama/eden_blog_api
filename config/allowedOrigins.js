4d2345989ee1b5bc4758c0712c28f90854203c0f
Amr Jamal <amrj.bagha@gmail.com>
2025-06-16 06:32:38 +0300

    update allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174", "https://eden-blog-client.vercel.app"];
+const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173", "http://localhost:5174", "http://127.0.0.1:5174", "https://eden-blog.vercel.app"];
 
 module.exports = allowedOrigins;
